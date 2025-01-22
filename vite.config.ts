a102772950b72a56df7dec7c871ac89ef870a530
akjmalamin7 <thpi205526@gmail.com>
2025-01-22 09:19:37 +0600

    refactor: clean up code formatting and improve TypeScript configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { defineConfig } from 'vite';
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'), // নিশ্চিত করুন সঠিক পথ ব্যবহার হচ্ছে
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
